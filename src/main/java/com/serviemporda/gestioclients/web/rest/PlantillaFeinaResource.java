7f8cf93e04396e3fa0a33d393ed4b92344ec38e3
carlaradresa <ls30899@salleurl.edu>
2020-04-21 15:46:38 +0200

    commit antes de cambios en jdl 18/04/2020

--- src/main/java/com/serviemporda/gestioclients/web/rest/PlantillaFeinaResource.java
+++ src/main/java/com/serviemporda/gestioclients/web/rest/PlantillaFeinaResource.java
@@ -48,6 +48,7 @@ public class PlantillaFeinaResource {
         this.feinaRepository = feinaRepository;
         this.plantillaFeinaRepository = plantillaFeinaRepository;
     }
+
     /**
      * {@code POST  /plantilla-feinas} : Create a new plantillaFeina.
      *
